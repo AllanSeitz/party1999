dd1dcde6f3932f88fe8322ddb2afdc5df6a67cb5
Allan <allan@Allans-MacBook-Pro.local>
2018-10-09 18:32:48 -0400

    inline styling

--- src/Movies.js
+++ src/Movies.js
@@ -1,26 +1,39 @@
 import React, { Component } from 'react'
+import styled from 'styled-components'
 import Data from './Data.json'
 
 class Movies extends Component {
   render() {
+    const Text = styled.p`
+      border: solid black 1px;
+      background-color: antiquewhite;
+      font-weight: bold;
+      margin-left: 20px;
+      margin-right: 175px;
+    `
+    const Page = styled.section`
+      display: flex;
+      padding-bottom: 10px;
+    `
+
     return (
       <>
         {Data.results.map(result => {
           return (
-            <section>
+            <Page>
               <img
                 src={`https://image.tmdb.org/t/p/w185_and_h278_bestv2${
                   result.poster_path
                 }`}
                 alt="some movies posters"
               />
-              <div className="text">
-                <p>
+              <div>
+                <Text>
                   <em>{result.title}</em>
-                </p>
-                <p>{result.overview} </p>
+                </Text>
+                <Text>{result.overview} </Text>
               </div>
-            </section>
+            </Page>
           )
         })}
       </>
